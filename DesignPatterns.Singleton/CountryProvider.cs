c0605afc06dee62b8e7bc815d36996f52853b169
nurselaltin <nurselaltin.na@gmail.com>
2024-03-28 01:54:25 +0300

    Starting to singleton pattern.

---
+++ DesignPatterns.Singleton/CountryProvider.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DesignPatterns.StrategyPattern.Models
+{
+  //Metot 2. kez çağırılıyor ve aynı veri için db'ye 2 kez gidiliyor.Arada 4 sn geçiyor.Burada bu zaman aralığını düşürebilir ve aynı veri tekrardan dbye gitmemize gerek kalmayacak şekilde singleton pattern kullanabiliriz.
+
+  // 1) 1. yöntem
+  //public class CountryProvider
+  //{
+  //  public async Task<List<Country>> GetCountries() 
+  //  {
+  //    //Retrieving data from db 
+  //    await Task.Delay(2000);
+
+  //    return new List<Country>
+  //    {
+  //       new Country(){Name = "Türkiye"},
+  //       new Country(){Name = "Kanada"}
+  //    };
+  //  }
+  //}
+
+  //2)***Bu instance ilk  eriştiğimde create edilsin
+  public class CountryProvider
+  {
+    private static CountryProvider instance = null; 
+    public static CountryProvider Instance {
+
+      get
+      {
+          //Tek yaratıldığından emin olalım.
+          if(instance is not null)
+            return instance;
+          else
+           return instance = new CountryProvider();
+
+      } 
+      set { instance = value; } 
+    }
+
+    public async Task<List<Country>> GetCountries()
+    {
+      //Retrieving data from db 
+      await Task.Delay(2000);
+
+      return new List<Country>
+      {
+         new Country(){Name = "Türkiye"},
+         new Country(){Name = "Kanada"}
+      };
+    }
+  }
+}
---
+++ DesignPatterns.Singleton/Program.cs
@@ -0,0 +1,37 @@
+﻿// See https://aka.ms/new-console-template for more information
+using DesignPatterns.StrategyPattern.Models;
+
+//1.Kullanım
+
+//Console.WriteLine("Başlangıç: " + DateTime.Now.ToLongTimeString());
+//var countryProvider = new CountryProvider();
+
+//var countries = await countryProvider.GetCountries();
+//foreach (var item in countries)
+//{
+//  Console.WriteLine(item.Name);
+//}
+
+////Another Service
+//var countryProvider2 = new CountryProvider();
+//var countries2 = await countryProvider.GetCountries();
+//Console.WriteLine("Bitiş: " + DateTime.Now.ToLongTimeString());
+
+
+//2) Daha güzeli
+Console.WriteLine("Başlangıç: " + DateTime.Now.ToLongTimeString());
+var countryProvider = new CountryProvider();
+var countries = await CountryProvider.Instance.GetCountries();
+
+foreach (var item in countries)
+{
+  Console.WriteLine(item.Name);
+}
+
+//Another Service
+var countryProvider2 = new CountryProvider();
+var countries2 = await CountryProvider.Instance.GetCountries();
+Console.WriteLine("Bitiş: " + DateTime.Now.ToLongTimeString());
+
+Console.ReadLine();
+
